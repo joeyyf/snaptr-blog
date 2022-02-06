9137a465d5250140dcb35f5e8624ef1e3ef270c1
Joey <snaptraxlive@live.de>
2022-02-06 11:25:34 +0100

    added tmux post template

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const basePath = process.env.NODE_ENV === 'production' ? '/snaptr-blog' : '';
 
 module.exports = {
   reactStrictMode: true,
-  basePath,
+  basePath: `${basePath}`,
   assetPrefix: `${basePath}/`
 }
